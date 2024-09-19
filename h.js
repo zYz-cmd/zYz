402aaf0c86046dd6e4db4b4f9efb0d1d7e42ba46
zYz <2468078438@qq.com>
2024-09-19 17:52:48 +0800

    baocun

--- h.js
+++ h.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 console.log("--------");
 console.log("主分支");
 console.log("--------");
 
 
-=======
 console.log("----------");
 
 console.log("子分支");
 
 console.log("-----------");
->>>>>>> dev
